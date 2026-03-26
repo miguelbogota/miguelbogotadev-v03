7e4054149855f70127ef0269e8248c20aa155f20
Miguel Bogota <miguelbogota.rico@outlook.com>
2026-03-25 21:52:02 -0500

    feature: Routing & Data Fetching (#2)

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,17 @@
 import { createRouter as createTanStackRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
+import type { RouterContext } from './types/router-context';
 
 export function getRouter() {
   const router = createTanStackRouter({
     routeTree,
-
     scrollRestoration: true,
     defaultPreload: 'intent',
     defaultPreloadStaleTime: 0,
+    context: {
+      content: null,
+      projects: [],
+    } as unknown as RouterContext,
   });
 
   return router;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,28 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 
-export const Route = createFileRoute('/')({ component: App });
+export const Route = createFileRoute('/')({
+  component: App,
+  loader: ({ context }) => context,
+});
 
 function App() {
-  return <main>hello</main>;
+  const { content, projects } = Route.useLoaderData();
+
+  return (
+    <main>
+      {JSON.stringify(content)}
+
+      {projects.map((project) => (
+        <div key={project.id}>
+          <h2>{project.displayName}</h2>
+          <p>{project.description}</p>
+          <p>{project.startedAt}</p>
+
+          <Link to="/project/$projectId" params={{ projectId: project.id }}>
+            View Project
+          </Link>
+        </div>
+      ))}
+    </main>
+  );
 }
